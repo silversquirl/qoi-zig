2e181f5cb1c082b901e2140c6fe9bccc5e30620e
Silver <silver@silv.re>
2022-03-07 13:13:33 +0000

    feat: implement streaming encoder

--- qoi.zig
+++ qoi.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
 
 pub fn readAlloc(allocator: std.mem.Allocator, r: anytype) !Image {
-    var dec = Decoder(@TypeOf(r)){ .r = r };
+    var dec = decoder(r);
     const header = try dec.readHeader();
     const pixels = try allocator.alloc([4]u8, @as(usize, header.width) * header.height);
     for (pixels) |*px| {
@@ -18,6 +18,15 @@ pub const Image = struct {
     pixels: [][4]u8,
 };
 
+pub fn write(w: anytype, header: Header, pixels: []const [4]u8) !void {
+    var enc = encoder(w);
+    try enc.writeHeader(header);
+    for (pixels) |px| {
+        try enc.writePixel(px);
+    }
+    try enc.writeEnd();
+}
+
 pub const Header = struct {
     width: u32,
     height: u32,
@@ -33,6 +42,9 @@ pub const ColorSpace = enum(u1) { srgb, linear };
 
 pub fn decoder(r: anytype) Decoder(@TypeOf(r)) {
     return .{ .r = r };
+}
+pub fn encoder(w: anytype) Encoder(@TypeOf(w)) {
+    return .{ .w = w };
 }
 
 pub fn Decoder(comptime R: type) type {
@@ -97,13 +109,140 @@ pub fn Decoder(comptime R: type) type {
         }
 
         pub fn readEnd(self: Self) !void {
-            const marker = [1]u8{0} ** 7 ++ [1]u8{1};
-            if (!try self.r.isBytes(&marker)) {
+            if (!try self.r.isBytes(&end_marker)) {
                 return error.InvalidEndMarker;
             }
         }
     };
 }
+
+pub fn Encoder(comptime W: type) type {
+    return struct {
+        w: W,
+        ppx: [4]u8 = .{ 0, 0, 0, 255 }, // Previous pixel
+        run: u6 = 0, // Current run length
+        map: [64][4]u8 = std.mem.zeroes([64][4]u8), // Pixel hash map
+
+        const Self = @This();
+
+        pub fn writeHeader(self: Self, hdr: Header) !void {
+            try self.w.writeAll("qoif");
+            try self.w.writeIntBig(u32, hdr.width);
+            try self.w.writeIntBig(u32, hdr.height);
+            try self.w.writeByte(@enumToInt(hdr.channels));
+            try self.w.writeByte(@enumToInt(hdr.colorspace));
+        }
+
+        pub fn writePixel(self: *Self, px: [4]u8) !void {
+            defer {
+                // Set previous pixel
+                self.ppx = px;
+                // Store pixel in hashmap
+                self.map[pixelHash(px)] = px;
+            }
+
+            if (std.meta.eql(px, self.ppx) and self.run < 62) {
+                // Add to the run length
+                self.run += 1;
+                return;
+            }
+
+            if (self.run > 0) {
+                // Write a RUN chunk
+                try self.w.writeByte(@as(u8, 0xc0) | (self.run - 1));
+                self.run = 0;
+
+                if (std.meta.eql(px, self.ppx)) {
+                    // This is the case where we overflowed one RUN chunk,
+                    // so we need to start another directly after
+                    self.run += 1;
+                    return;
+                }
+            }
+
+            const idx = pixelHash(px);
+            if (std.meta.eql(px, self.map[idx])) {
+                // Write an INDEX chunk
+                try self.w.writeByte(@as(u8, 0x00) | idx);
+                return;
+            }
+
+            if (px[3] == self.ppx[3]) {
+                const dr = @bitCast(i8, px[0] -% self.ppx[0]);
+                const dg = @bitCast(i8, px[1] -% self.ppx[1]);
+                const db = @bitCast(i8, px[2] -% self.ppx[2]);
+
+                if (dr > -3 and dr < 2 and
+                    dg > -3 and dg < 2 and
+                    db > -3 and db < 2)
+                {
+                    // Write a DIFF chunk
+                    try self.w.writeByte(@as(u8, 0x40) |
+                        @intCast(u8, dr + 2) << 4 |
+                        @intCast(u8, dg + 2) << 2 |
+                        @intCast(u8, db + 2));
+                    return;
+                }
+
+                const dr_g = dr -% dg;
+                const db_g = db -% dg;
+                if (dr_g > -9 and dr_g < 8 and
+                    dg > -33 and dg < 32 and
+                    db_g > -9 and db_g < 8)
+                {
+                    // Write a LUMA chunk
+                    try self.w.writeByte(@as(u8, 0x80) | @intCast(u8, dg + 32));
+                    try self.w.writeByte(@intCast(u8, dr_g + 8) << 4 | @intCast(u8, db_g + 8));
+                    return;
+                }
+
+                // Write RGB chunk
+                try self.w.writeByte(0xfe);
+                try self.w.writeAll(px[0..3]);
+            } else {
+                // Write RGBA chunk
+                try self.w.writeByte(0xff);
+                try self.w.writeAll(px[0..4]);
+            }
+        }
+
+        fn diff(px: [4]u8, ppx: [4]u8) ?u6 {
+            return (dpx(px[0], ppx[0]) orelse return null) << 4 |
+                (dpx(px[1], ppx[1]) orelse return null) << 2 |
+                (dpx(px[2], ppx[2]) orelse return null);
+        }
+        fn dpx(c: u8, pc: u8) ?u6 {
+            if (pc > c) {
+                if (pc - c > 2) return null;
+                return 2 - @intCast(u2, pc - c);
+            } else {
+                if (c - pc > 1) return null;
+                return 2 + @intCast(u2, c - pc);
+            }
+        }
+
+        fn luma(px: [4]u8, ppx: [4]u8) ?Luma {
+            _ = px;
+            _ = ppx;
+            return null; // TODO
+        }
+        const Luma = struct {
+            g: u6,
+            rb: u8,
+        };
+
+        pub fn writeEnd(self: Self) !void {
+            if (self.run > 0) {
+                // Write the last RUN chunk
+                try self.w.writeByte(@as(u8, 0xc0) | (self.run - 1));
+            }
+
+            try self.w.writeAll(&end_marker);
+        }
+    };
+}
+
+const end_marker = [1]u8{0} ** 7 ++ [1]u8{1};
 
 fn pixelHash(px: [4]u8) u6 {
     const hash =
--- test.zig
+++ test.zig
@@ -24,6 +24,28 @@ test "decode: wikipedia_008" {
     try testDecode("wikipedia_008");
 }
 
+test "encode: dice" {
+    try testEncode("dice");
+}
+test "encode: kodim10" {
+    try testEncode("kodim10");
+}
+test "encode: kodim23" {
+    try testEncode("kodim23");
+}
+test "encode: qoi_logo" {
+    try testEncode("qoi_logo");
+}
+test "encode: testcard" {
+    try testEncode("testcard");
+}
+test "encode: testcard_rgba" {
+    try testEncode("testcard_rgba");
+}
+test "encode: wikipedia_008" {
+    try testEncode("wikipedia_008");
+}
+
 fn testDecode(comptime stem: []const u8) !void {
     const path_qoi = std.fmt.comptimePrint("qoi_test_images/{s}.qoi", .{stem});
     const path_png = std.fmt.comptimePrint("qoi_test_images/{s}.png", .{stem});
@@ -54,5 +76,71 @@ fn testDecode(comptime stem: []const u8) !void {
             @intCast(u8, px[0] >> 8), @intCast(u8, px[1] >> 8),
             @intCast(u8, px[2] >> 8), @intCast(u8, px[3] >> 8),
         }, img_qoi.pixels[i]);
+    }
+}
+
+fn testEncode(comptime stem: []const u8) !void {
+    const path = std.fmt.comptimePrint("qoi_test_images/{s}.qoi", .{stem});
+
+    const input = try std.fs.cwd().readFileAlloc(std.testing.allocator, path, 10 << 20);
+    defer std.testing.allocator.free(input);
+    const img = try qoi.readAlloc(std.testing.allocator, std.io.fixedBufferStream(input).reader());
+    defer std.testing.allocator.free(img.pixels);
+
+    var buf = std.ArrayList(u8).init(std.testing.allocator);
+    defer buf.deinit();
+    try qoi.write(buf.writer(), img.header, img.pixels);
+
+    try expectEqualBytes(input, buf.items);
+}
+
+fn expectEqualBytes(expected: []const u8, actual: []const u8) !void {
+    for (actual) |b, i| {
+        if (i >= expected.len) {
+            break;
+        }
+        if (expected[i] != b) {
+            std.debug.print("difference at byte 0x{x}: expected 0x{x}, found 0x{x}\n", .{ i, expected[i], b });
+
+            const line_start = i & ~@as(usize, 15);
+            const start = line_start -| 16;
+            const end = line_start +| 2 * 16;
+            hexDump(start, expected[start..@minimum(end, expected.len)]);
+            std.debug.print("{s}\n", .{"-" ** 69});
+            hexDump(start, actual[start..@minimum(end, actual.len)]);
+
+            return error.TestExpectedEqual;
+        }
+    }
+    if (expected.len != actual.len) {
+        std.debug.print("lengths differ: expected {}, found {}\n", .{ expected.len, actual.len });
+        return error.TestExpectedEqual;
+    }
+}
+fn hexDump(offset: usize, bytes: []const u8) void {
+    var base: usize = 0;
+    while (base < bytes.len) : (base += 16) {
+        const end = base + 16;
+
+        std.debug.print("{x:0>8}:", .{base + offset});
+        var i = base;
+        while (i < end) : (i += 1) {
+            if (i & 7 == 0) std.debug.print(" ", .{});
+            if (i & 1 == 0) std.debug.print(" ", .{});
+            if (i < bytes.len) {
+                std.debug.print("{x:0>2}", .{bytes[i]});
+            } else {
+                std.debug.print("  ", .{});
+            }
+        }
+
+        std.debug.print("  ", .{});
+        for (bytes[base..@minimum(end, bytes.len)]) |b| {
+            std.debug.print("{c}", .{
+                if (std.ascii.isPrint(b)) b else '.',
+            });
+        }
+
+        std.debug.print("\n", .{});
     }
 }
